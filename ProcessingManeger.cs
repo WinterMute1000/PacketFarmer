9fc4da5ef164e44a29b7506abd9bcc8af8036f6d
WinterMute1000 <42152852+WinterMute1000@users.noreply.github.com>
2019-05-18 02:45:25 +0900

    Make module define
    module define

---
+++ ProcessingManeger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacketFarmer
+{
+	class ProcessingManeger
+	{
+		public void pcapError() //Print error 
+		{
+
+		}
+
+		public void FindInterface() { } //Interface select
+		public void openInterface() { } //Interface device open
+		//public Func<> caughtPacket;
+	}
+}
--- UIManager.cs
+++ UIManager.cs
@@ -8,6 +8,29 @@ namespace PacketFarmer
 {
 	class UIManager
 	{
+		public void saveResult() //Save Result(selected path)
+		{
+		}
+		public void selectProtocol()
+		{
 
+		}//Send event to Processing Maneger for change protocol
+
+		public void stopCapture()
+		{
+
+		} // Stop packet capture event send to Processing Maneger
+		public void setPacketCaptureNum ()
+		{
+
+		} //Send packet num to Processing Maneger
+
+		public void updateScrollView() //Add result
+		{
+
+		}
+		public void exitProgram() //End Program
+		{
+		}
 	}
 }
